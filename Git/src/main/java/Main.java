fa1b8a99c0fb1c00731dc9568e4373e3ccc26869
ncano <nicolas.cano@emminens.com>
2017-05-31 17:09:29 +0200

    Add javadoc

--- Git/src/main/java/Main.java
+++ Git/src/main/java/Main.java
@@ -4,6 +4,11 @@ public class Main {
 	
 	public static String msg = "feature 2";
 
+	/**
+	 * Main method
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// Comment in release 1
 		System.out.println("Hello! " + msg);
